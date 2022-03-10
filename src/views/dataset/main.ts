642871774dbc7f2e430051f14113c842ff965d3c
Frank YT Chen 陳沿廷 <frank.yt.chen@auo.com>
2022-03-10 14:03:59 +0800

    flow node change

--- src/views/dataset/main.ts
+++ src/views/dataset/main.ts
@@ -4,4 +4,31 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class Dataset extends Vue {
 
+    private selectedDatasetValue = '';
+    private datasetOptions  = [
+        {
+          value: "all data",
+          label: "所有資料集",
+        },
+        {
+          value: "dataset 001",
+          label: "001 dataset",
+        },
+        {
+          value: "dataset 002",
+          label: "002 dataset",
+        },
+        {
+          value: "dataset 003",
+          label: "003 dataset",
+        },
+        {
+          value: "dataset 004",
+          label: "004 dataset",
+        },
+        {
+          value: "dataset 005",
+          label: "005 dataset",
+        },
+      ];
 }
