04aa3813bbc36d804b48c8dfa2e60c3a1dad1c31
Frank YT Chen 陳沿廷 <frank.yt.chen@auo.com>
2022-03-14 12:48:50 +0800

    add switch case func to processCell data

--- src/io/processCellData.ts
+++ src/io/processCellData.ts
@@ -4,10 +4,38 @@ export default class ProcessCellData {
     component!: string;
     content!: string[];
 
-    static ofPostProcess(component: string, experiment: Experiment): ProcessCellData {
+    static cellDataContent(component: string, experiment: Experiment): ProcessCellData {
+
+        let contentData: string[] = []
+
+        switch (component) {
+            case "dataset-node":
+                break;
+            case "preprocess-node":
+                contentData = Object.keys(experiment.ConfigPreprocess.PreprocessPara)
+                break;
+            case "data-argument-node":
+                contentData = Object.keys(experiment.ConfigAugmentation.AugmentationPara)
+                break;
+            case "model-select-node":
+                contentData.push(experiment.ConfigPytorchModel.SelectedModel.model?.structure ?? '')
+                break;
+            case "validation-select-node":
+                break;
+            case "trained-result-node":
+                break;
+            case "test-result-node":
+                break;
+            default:
+                break;
+        }
+
+
+        // console.log("experiment",experiment)
         return {
             component,
-            content: [],
+            content: contentData,
         }
     }
+
 }
