5c3bf2c2b96f5a2776408aeb389c8f4d9a8f2cab
Frank YT Chen 陳沿廷 <frank.yt.chen@auo.com>
2022-03-15 09:44:43 +0800

    clean confict

--- src/io/processCellData.ts
+++ src/io/processCellData.ts
@@ -4,41 +4,6 @@ export default class ProcessCellData {
     component!: string;
     content!: string[];
 
-<<<<<<< HEAD
-    static cellDataContent(component: string, experiment: Experiment): ProcessCellData {
-
-        let contentData: string[] = []
-
-        switch (component) {
-            case "dataset-node":
-                contentData = ["未上傳","未標記","未切分"]
-                break;
-            case "preprocess-node":
-                contentData = Object.keys(experiment.ConfigPreprocess.PreprocessPara)
-                break;
-            case "data-argument-node":
-                contentData = Object.keys(experiment.ConfigAugmentation.AugmentationPara)
-                break;
-            case "model-select-node":
-                contentData.push(experiment.ConfigPytorchModel.SelectedModel.model?.structure ?? '')
-                break;
-            case "validation-select-node":
-                contentData = Object.keys(experiment.ConfigEvaluation.EvaluationPara)
-                break;
-            case "trained-result-node":
-                contentData.push("尚未訓練")
-                break;
-            case "test-result-node":
-                contentData.push("尚未訓練")
-                break;
-            default:
-                break;
-        }
-        return {
-            component,
-            content: contentData,
-        }
-=======
     static cellDataContent(experiment: Experiment): Map<string, ProcessCellData> {
         experiment;
         return new Map([
@@ -71,7 +36,6 @@ export default class ProcessCellData {
                 content: [],
             }],
         ]);
->>>>>>> 8cfde832b54693db03ce1562db32557a5a9e1b90
     }
 
 }
