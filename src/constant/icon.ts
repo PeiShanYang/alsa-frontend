d9902e4ade018c5c2113281664418fd6f965b580
Frank YT Chen 陳沿廷 <frank.yt.chen@auo.com>
2022-05-12 17:35:46 +0800

    add new icon

--- src/constant/icon.ts
+++ src/constant/icon.ts
@@ -1,12 +1,31 @@
-import DatasetIcon from '@/assets/Forallvision_icon0304/pipe_dataset.svg'
-import PreprocessIcon from '@/assets/Forallvision_icon0304/pipe_preprocess.svg'
-import DataArgumentIcon from '@/assets/Forallvision_icon0304/pipe_data_argument.svg'
-import ModelSelectIcon from '@/assets/Forallvision_icon0304/pipe_model_select.svg'
-import ValidationSelectIcon from '@/assets/Forallvision_icon0304/pipe_validation_select.svg'
-import TrainedResultIcon from '@/assets/Forallvision_icon0304/pipe_trained_result.svg'
-import TestedResultIcon from '@/assets/Forallvision_icon0304/pipe_tested_result.svg'
+import Advantage01 from '@/assets/svg_icon/dashboard/advantage_from_project_manage_view.svg'
+import Advantage02 from '@/assets/svg_icon/dashboard/advantage_instance_process.svg'
+import Advantage03 from '@/assets/svg_icon/dashboard/advantage_large_resource_and_easy_operate.svg'
+
+import EditIcon from '@/assets/svg_icon/dataset/edit.svg'
+
+
+import DatasetIcon from '@/assets/svg_icon/experiments/dataset.svg'
+import PreprocessIcon from '@/assets/svg_icon/experiments/preprocess.svg'
+import DataArgumentIcon from '@/assets/svg_icon/experiments/data_argument.svg'
+import ModelSelectIcon from '@/assets/svg_icon/experiments/model_select.svg'
+import ValidationSelectIcon from '@/assets/svg_icon/experiments/validation_select.svg'
+import TrainedResultIcon from '@/assets/svg_icon/experiments/trained_result.svg'
+import TestedResultIcon from '@/assets/svg_icon/experiments/tested_result.svg'
+
+
 
 export default class Icons {
+    static readonly dataset = DatasetIcon;
+    static readonly preprocess = PreprocessIcon;
+    static readonly dataAugmentation = DataArgumentIcon;
+    static readonly modelSelect = ModelSelectIcon;
+    static readonly validationSelect = ValidationSelectIcon;
+    static readonly trainedResult = TrainedResultIcon;
+    static readonly testedResult = TestedResultIcon;
+}
+
+export class ExperimentIcons{
     static readonly dataset = DatasetIcon;
     static readonly preprocess = PreprocessIcon;
     static readonly dataAugmentation = DataArgumentIcon;
