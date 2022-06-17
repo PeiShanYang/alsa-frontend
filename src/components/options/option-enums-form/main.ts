49daf380507eb9136a8a61f3b5b9a7db9b2537c1
JoeJHHuang <joe.jh.huang@auo.com>
2022-06-17 00:18:02 +0000

    add option enums form

--- src/components/options/option-enums-form/main.ts
+++ src/components/options/option-enums-form/main.ts
@@ -1,30 +1,38 @@
-import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
+import { Component, Emit, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component
 export default class OptionEnumsForm extends Vue {
   @Prop() private enums!: Map<string, string | number>
   @Prop() private value!: string | number
 
-  private v = this.value
+  private valueName = this.getKeyName
 
   private get enumsMap(): Map<string, string | number> {
-    console.log(this.enums)
-    return new Map<string, string | number>(this.enums)
+    return new Map<string, string | number>(Object.entries(this.enums))
   }
 
-  private get keyName(): string {
+  private get getKeyName(): string {
     for (const key in this.enumsMap.keys()) {
-      if (this.enums.get(key) === this.v) return key
+      if (this.enums.get(key) === this.value) return key
     }
     return ''
   }
 
-  private handleDropdownDisplay(command: string): void {
-    this.v = this.enumsMap.get(command) ?? ''
+  private displayName(name: string): string {
+    return this.$i18n.t(name).toString()
   }
 
-  // @Emit("input")
-  // private onInputChange(): string | number {
-  //   return this.v
-  // }
+  private handleDropdownDisplay(command: string): void {
+    this.valueName = command
+  }
+
+  @Watch('value')
+  private onValueChange() {
+    this.valueName = this.getKeyName
+  }
+
+  @Emit("input")
+  private onInputChange(): string | number {
+    return this.enumsMap.get(this.valueName) ?? ''
+  }
 }
