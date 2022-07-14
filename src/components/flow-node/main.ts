9ef6d4777ee075e6151be43b963cdb2cb8573334
Frank YT Chen 陳沿廷 <frank.yt.chen@auo.com>
2022-07-14 14:13:13 +0800

    fix tree-node, tree-list-node ui display

--- src/components/flow-node/main.ts
+++ src/components/flow-node/main.ts
@@ -14,8 +14,8 @@ export default class flowNode extends Vue {
 
   @Inject("getNode") private getNode!: () => VueShape;
 
-  private nodeBackgroundColor = `background: ${this.backgroundColor}`;
-  private nodeBorderColor = `border-color: ${this.borderColor}; border-left:2px solid ${this.borderColor};opacity: ${this.opacity};`;
+  private nodeBackgroundColor = `background: ${this.backgroundColor};border-color: ${this.borderColor};opacity: ${this.opacity};`;
+  private nodeBorderColor = `border-color: ${this.borderColor};opacity: ${this.opacity};`;
   private nodeContent = ["暫無資料"]
   
 
