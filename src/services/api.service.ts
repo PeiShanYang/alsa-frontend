fb062591dddc7eb7e59157aee02dc06645587ffb
JoeJHHuang <joe.jh.huang@auo.com>
2022-08-17 09:30:42 +0000

    push dist to gitlab

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -45,7 +45,8 @@ import { GetModelPreTrainedWeightReq, GetModelPreTrainedWeightRes } from "@/io/r
 
 // const host = 'http://tw100104318:37510/';
 // const host = 'http://tw100104318:65101/';
-const host = 'https://adt-sala.azurewebsites.net:5000/'
+// const host = 'https://adt-sala.azurewebsites.net:5000/'
+const host = 'http://localhost:5000/'
 
 
 export default class Api {
